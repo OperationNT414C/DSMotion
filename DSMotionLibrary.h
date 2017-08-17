cb17e37a346a64e35784b581ff109c3e28df5966
OperationNT414C <OperationNT414C@users.noreply.github.com>
2017-08-17 21:42:32 +0200

    Update DSMotionLibrary.h

--- DSMotionLibrary.h
+++ DSMotionLibrary.h
@@ -1,8 +1,19 @@
 #ifndef DSMotionLibrary_H
 #define DSMotionLibrary_H
 
+struct accelGyroData
+{
+    signed short accel[3];
+    signed short gyro[3];
+    unsigned int timestamp;
+    unsigned int counter;
+};
+
+unsigned int dsGetCurrentTimestamp();
+unsigned int dsGetCurrentCounter();
+
 int dsResetAccelGyroSampling();
 int dsGetSampledAccelGyro(signed short oAccel[3], signed short oGyro[3]);
-int dsGetInstantAccelGyro(signed short oAccel[3], signed short oGyro[3]);
+int dsGetInstantAccelGyro(unsigned int iIndex, struct accelGyroData* oData);
 
 #endif
