719286e428c2d70357c2487844cb9a883719052b
OperationNT414C <OperationNT414C@users.noreply.github.com>
2017-08-19 16:17:29 +0200

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -32,13 +32,86 @@
 #include <string.h>
 #include "../DSMotionLibrary.h"
 
+
 extern unsigned int ksceKernelGetSystemTimeLow();
 
-#define DS4_VID   0x054C
+#define abs(x) (((x) < 0) ? -(x) : (x))
+
+#define SONY_VID 0x054C
+
+#define DS3_PID 0x0268
+
+struct ds3_input_report {
+	unsigned char report_id;
+	unsigned char unk0;
+
+	unsigned char select : 1;
+	unsigned char l3     : 1;
+	unsigned char r3     : 1;
+	unsigned char start  : 1;
+	unsigned char up     : 1;
+	unsigned char right  : 1;
+	unsigned char down   : 1;
+	unsigned char left   : 1;
+
+	unsigned char l2       : 1;
+	unsigned char r2       : 1;
+	unsigned char l1       : 1;
+	unsigned char r1       : 1;
+	unsigned char triangle : 1;
+	unsigned char circle   : 1;
+	unsigned char cross    : 1;
+	unsigned char square   : 1;
+
+	unsigned char ps       : 1;
+	unsigned char not_used : 7;
+
+	unsigned char unk1;
+
+	unsigned char left_x;
+	unsigned char left_y;
+	unsigned char right_x;
+	unsigned char right_y;
+
+	unsigned int unk2;
+
+	unsigned char up_sens;
+	unsigned char right_sens;
+	unsigned char down_sens;
+	unsigned char left_sens;
+
+	unsigned char L2_sens;
+	unsigned char R2_sens;
+	unsigned char L1_sens;
+	unsigned char R1_sens;
+
+	unsigned char triangle_sens;
+	unsigned char circle_sens;
+	unsigned char cross_sens;
+	unsigned char square_sens;
+
+	unsigned short unk3;
+	unsigned char unk4;
+
+	unsigned char status;
+	unsigned char power_rating;
+	unsigned char comm_status;
+	unsigned int unk5;
+	unsigned int unk6;
+	unsigned char unk7;
+
+	unsigned short accel_x;
+	unsigned short accel_y;
+	unsigned short accel_z;
+
+	union {
+		unsigned short gyro_z;
+		unsigned short roll;
+	};
+} __attribute__((packed, aligned(32)));
+
 #define DS4_PID   0x05C4
 #define DS4_2_PID 0x09CC
-
-#define abs(x) (((x) < 0) ? -(x) : (x))
 
 struct ds4_input_report {
 	unsigned char report_id;
@@ -115,22 +188,21 @@ struct ds4_input_report {
 
 } __attribute__((packed, aligned(32)));
 
-static int ds4_connected = 0;
-static unsigned int ds4_mac0 = 0;
-static unsigned int ds4_mac1 = 0;
+static int ds3_connected = 0;
+static struct ds3_input_report ds3_input;
 
+static int ds4_connected = 0;
 static struct ds4_input_report ds4_input;
+
 static unsigned char* recv_buff = NULL;
+static unsigned int ds_mac0 = 0;
+static unsigned int ds_mac1 = 0;
 
 #define NB_DATA 64
 
 static struct accelGyroData previousData[NB_DATA];
 static int currentData = NB_DATA-1;
 static int globalCounter = 0;
-
-static int accel_sum[3] = {0, 0, 0};
-static int gyro_sum[3] = {0, 0, 0};
-static int samplesCount = 0;
 
 unsigned int dsGetCurrentTimestamp()
 {
@@ -142,50 +214,50 @@ unsigned int dsGetCurrentCounter()
     return globalCounter;
 }
 
-int dsResetAccelGyroSampling()
+unsigned int dsGetSampledAccelGyro(unsigned int iSamplingTimeMS, signed short oAccel[3], signed short oGyro[3])
 {
-    if (!ds4_connected)
-        return -1;
+    if (!ds3_connected && !ds4_connected)
+        return 0;
 
-    for (int i = 0; i < 3; i++)
+    int initIndex = currentData;
+    struct accelGyroData* data = &previousData[initIndex];
+
+    int accel_sum[3] = {data->accel[0], data->accel[1], data->accel[2]};
+    int gyro_sum[3] = {data->gyro[0], data->gyro[1], data->gyro[2]};
+
+    unsigned int initTime = data->timestamp;
+    unsigned int samplingTimeNano = 1000 * iSamplingTimeMS;
+
+    signed short index;
+    for (index = 1; index < NB_DATA; index++)
     {
-        accel_sum[i] = 0;
-        gyro_sum[i] = 0;
-    }
-
-    samplesCount = 0;
-
-    return 0;
-}
-
-int dsGetSampledAccelGyro(signed short oAccel[3], signed short oGyro[3])
-{
-    if (!ds4_connected)
-        return -1;
-    
-    if (samplesCount > 0)
-    {
-        signed short accel[3] = {accel_sum[0] / samplesCount, accel_sum[1] / samplesCount, accel_sum[2] / samplesCount};
-        signed short gyro[3] = {gyro_sum[0] / samplesCount, gyro_sum[1] / samplesCount, gyro_sum[2] / samplesCount};
-    
-        ksceKernelMemcpyKernelToUser((uintptr_t)oAccel, (const void *)accel, sizeof(accel));
-        ksceKernelMemcpyKernelToUser((uintptr_t)oGyro, (const void *)gyro, sizeof(gyro));
+        int dataIndex = (initIndex-index+NB_DATA)%NB_DATA;
+        data = &previousData[dataIndex];
         
-        dsResetAccelGyroSampling();
-    }
-    else
-    {
-        struct accelGyroData* data = &previousData[currentData];
-        ksceKernelMemcpyKernelToUser((uintptr_t)oAccel, (const void *)data->accel, 3*sizeof(signed short));
-        ksceKernelMemcpyKernelToUser((uintptr_t)oGyro, (const void *)data->gyro, 3*sizeof(signed short));
+        if (0 == data->counter || initTime-data->timestamp > samplingTimeNano)
+            break;
+        
+        accel_sum[0] += data->accel[0];
+        accel_sum[1] += data->accel[1];
+        accel_sum[2] += data->accel[2];
+
+        gyro_sum[0] += data->gyro[0];
+        gyro_sum[1] += data->gyro[1];
+        gyro_sum[2] += data->gyro[2];
     }
     
-    return 0;
+    signed short accel[3] = {accel_sum[0] / index, accel_sum[1] / index, accel_sum[2] / index};
+    signed short gyro[3] = {gyro_sum[0] / index, gyro_sum[1] / index, gyro_sum[2] / index};
+
+    ksceKernelMemcpyKernelToUser((uintptr_t)oAccel, (const void *)accel, 3*sizeof(signed short));
+    ksceKernelMemcpyKernelToUser((uintptr_t)oGyro, (const void *)gyro, 3*sizeof(signed short));
+
+    return index;
 }
 
 int dsGetInstantAccelGyro(unsigned int iIndex, struct accelGyroData* oData)
 {
-    if (!ds4_connected)
+    if (!ds3_connected && !ds4_connected)
         return -1;
 
     int curIndex = (currentData-(iIndex%NB_DATA)+NB_DATA)%NB_DATA;
@@ -194,15 +266,24 @@ int dsGetInstantAccelGyro(unsigned int iIndex, struct accelGyroData* oData)
     return 0;
 }
 
+static inline void ds3_input_reset(void)
+{
+	memset(&ds3_input, 0, sizeof(ds3_input));
+}
+
 static inline void ds4_input_reset(void)
 {
 	memset(&ds4_input, 0, sizeof(ds4_input));
 }
 
+static int is_ds3(const unsigned short vid_pid[2])
+{
+	return vid_pid[0] == SONY_VID && vid_pid[1] == DS3_PID;
+}
+
 static int is_ds4(const unsigned short vid_pid[2])
 {
-	return (vid_pid[0] == DS4_VID) &&
-		((vid_pid[1] == DS4_PID) || (vid_pid[1] == DS4_2_PID));
+	return (vid_pid[0] == SONY_VID) && ((vid_pid[1] == DS4_PID) || (vid_pid[1] == DS4_2_PID));
 }
 
 #define DECL_FUNC_HOOK(name, ...) \
@@ -221,60 +302,89 @@ DECL_FUNC_HOOK(SceBt_ksceBtReadEvent, SceBtEvent *events, int num_events)
             SceBtEvent* event = &events[i];
             //LOG("Connection event %d with %d %d\n", event->id, event->mac0, event->mac1);
 
-            if (!ds4_connected && 0x05 == event->id)
+            if (!ds3_connected && !ds4_connected && 0x05 == event->id)
             {
                 unsigned short vid_pid[2];
-                ksceBtGetVidPid(event->mac0, event->mac1, vid_pid);
+                unsigned int result1 = ksceBtGetVidPid(event->mac0, event->mac1, vid_pid);
                 //LOG("Vendor ID %d ; Product ID %d\n", vid_pid[0], vid_pid[1]);
+                //log_flush();
 
+                int connected = 0;
                 if (is_ds4(vid_pid))
                 {
-                    ds4_mac0 = event->mac0;
-                    ds4_mac1 = event->mac1;
                     ds4_input_reset();
                     ds4_connected = 1;
-                    
-                    dsResetAccelGyroSampling();
+                    connected = 1;
+                }
+                else
+                {
+                    char name[0x79];
+                    unsigned int result2 = ksceBtGetDeviceName(event->mac0, event->mac1, name);
+                    if (is_ds3(vid_pid)|| (result1 == 0x802F5001 && result2 == 0x802F0C01))
+                    {
+                        ds3_input_reset();
+                        ds3_connected = 1;
+                        connected = 1;
+                    }
+                }
+                
+                if (connected)
+                {
+                    ds_mac0 = event->mac0;
+                    ds_mac1 = event->mac1;
                     globalCounter = 0;
                 }
             }
-            else if (ds4_connected && event->mac0 == ds4_mac0 && event->mac1 == ds4_mac1)
+            else if ((ds3_connected || ds4_connected) && event->mac0 == ds_mac0 && event->mac1 == ds_mac1)
             {
                 if (0x06 == event->id)
+                {
+                    ds3_connected = 0;
                     ds4_connected = 0;
+                }
                 else if (NULL != recv_buff)
                 {
                     if (0x0A == event->id)
                     {
-                        if (0x11 == recv_buff[0])
+                        if ((ds4_connected && 0x11 == recv_buff[0]) || (ds3_connected && 0x01 == recv_buff[0]))
                         {
-                            memcpy(&ds4_input, recv_buff, sizeof(ds4_input));
+                            int newData = (currentData+1)%NB_DATA;
+                            struct accelGyroData* data = &previousData[newData];
 
-                            currentData = (currentData+1)%NB_DATA;
-                            struct accelGyroData* data = &previousData[currentData];
-                            
-                            data->accel[0] = ds4_input.accel_x;
-                            data->accel[1] = ds4_input.accel_y;
-                            data->accel[2] = ds4_input.accel_z;
+                            if (ds4_connected)
+                            {
+                                memcpy(&ds4_input, recv_buff, sizeof(ds4_input));
 
-                            data->gyro[0] = ds4_input.gyro_x;
-                            data->gyro[1] = ds4_input.gyro_y;
-                            data->gyro[2] = ds4_input.gyro_z;
+                                // Data from gyroscope and accelerometer seem inverted on DS4
+                                data->accel[0] = ds4_input.gyro_x;
+                                data->accel[1] = ds4_input.gyro_y;
+                                data->accel[2] = ds4_input.gyro_z;
+
+                                data->gyro[0] = ds4_input.accel_x;
+                                data->gyro[1] = ds4_input.accel_y;
+                                data->gyro[2] = ds4_input.accel_z;
+                            }
+                            else // if (ds3_connected)
+                            {
+                                memcpy(&ds3_input, recv_buff, sizeof(ds3_input));
+
+                                // DS3 matching with DS4
+                                data->accel[0] = -((signed short)ds3_input.accel_y)/4;
+                                data->accel[1] = -((signed short)ds3_input.accel_z)/4;
+                                data->accel[2] = ((signed short)ds3_input.accel_x)/4;
+
+                                data->gyro[0] = 0;
+                                data->gyro[1] = ((signed short)ds3_input.gyro_z+0x15FF)/10;
+                                data->gyro[2] = 0;
+                            }
 
                             data->timestamp = ksceKernelGetSystemTimeLow();
-                            data->counter = globalCounter++;
-                            
-                            for (int i = 0; i < 3; i++)
-                            {
-                                accel_sum[i] = data->accel[i];
-                                gyro_sum[i] = data->gyro[i];
-                            }
-                            
-                            samplesCount++;
+                            data->counter = (++globalCounter);
+                            currentData = newData;
                         }
                         recv_buff = NULL;
                     }
-                    else if (0x0B == event->id)
+                    else if (0x0B == event->id || 0x0C == event->id)
                         recv_buff = NULL;
                 }
             }
@@ -288,9 +398,9 @@ DECL_FUNC_HOOK(SceBt_ksceBtHidTransfer, unsigned int mac0, unsigned int mac1, Sc
 {
 	int ret = TAI_CONTINUE(int, SceBt_ksceBtHidTransfer_ref, mac0, mac1, request);
 
-    if (ret >= 0 && ds4_connected && mac0 == ds4_mac0 && mac1 == ds4_mac1)
+    if (ret >= 0 && (ds3_connected || ds4_connected) && mac0 == ds_mac0 && mac1 == ds_mac1)
     {
-        if (NULL != request && NULL != request->buffer && request->length >= sizeof(ds4_input))
+        if (NULL != request && NULL != request->buffer && request->length >= (ds4_connected?sizeof(ds4_input):sizeof(ds3_input)))
             recv_buff = (unsigned char*)request->buffer;
         else
             recv_buff = NULL;
